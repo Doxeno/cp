f40613586f1a57b062353eedeeb3dc8aa6e3ee03
Doxeno <47794695+Doxeno@users.noreply.github.com>
2021-02-15 21:32:44 +0100

    Update ntt.cpp

--- ntt.cpp
+++ ntt.cpp
@@ -1,3 +1,4 @@
+
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
@@ -25,6 +26,9 @@ ll fexp(ll b, ll e){
 //Computes P(x) for each N-th root of unity
 //rt is a primitive N-th root of unity
 
+//Computes the coefficients of a polynomial of degree N knowing its point values in the N-th roots of unity
+//rt is the inverse of a primitive N-th root of unity
+
 void ntt(vll& v, ll rt){
 	ll N = v.size();
 	if(N == 1)return;
@@ -43,34 +47,21 @@ void ntt(vll& v, ll rt){
 	}
 }
 
-//Recursive algorithm for inverse fft
-//Computes the coefficients of a polynomial of degree N knowing its point values in the N-th roots of unity
-//rt is the inverse of a primitive N-th root of unity
 
-void inverse_ntt(vll& v, ll rt){
-	ll N = v.size();
-	if(N == 1)return;
-	vll p(N/2);
-	vll d(N/2);
-	for(int i = 0; i < N; i++){
-		if(i%2)d[i/2] = v[i];
-		else p[i/2] = v[i];
-	}
-	ll w =1;
-	inverse_ntt(d,mult(rt,rt)); inverse_ntt(p,mult(rt,rt));
-	for(int i = 0; i < N/2; i++){
-		v[i] = (mod +p[i] + mult(w,d[i]))%mod;
-		v[i+N/2] = (mod+p[i] - mult(w,d[i]))%mod;
-		w = mult(w,rt);
-	}
-}
+
 
 //The order in which roots of unity are in v for both ntt and inverse ntt depends on rt
 //v[i] = rt^(i+1)
 
-//Finds the smallest N-th primitive root of unity
-ll find_root(ll N){ //size of the array, must be a power of 2
-    for(ll i = 1; i < mod; i++){
-        if(fexp(i,N) == 1 && fexp(i,N)!=1)return i;
-    }
+//multiplies 2 polynomials in nlogn
+vll multiply(vll a, vll b){
+	ll sz = 1;
+	while(sz < a.size() + b.size())sz*=2;
+	a.resize(sz); b.resize(sz);
+	ll rt = fexp(5,mod/sz);
+	ntt(a,rt); ntt(b,rt);
+	for(int i = 0; i < sz; i++)a[i] = mult(a[i],b[i]);
+	ntt(a,fexp(rt,mod-2));
+	for(auto &x: a)x = mult(x,fexp(sz,mod-2));
+	return a;
 }
